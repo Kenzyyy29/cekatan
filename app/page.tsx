c80649604279fa19e43fbd373f02896c6d1b94d0
Kenzyyy29 <kenzyworld29@gmail.com>
2025-01-18 22:03:19 +0700

    update:home

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,46 @@
 import Link from "next/link";
+import CardList from "@/components/CardList/OnlineHours";
+import InstantProcess from "@/components/CardList/InstantProcess"
+import Features from "@/components/CardList/Features";
+import MaxSpeed from "@/components/CardList/MaxSpeed";
 
 export default function Home() {
-  return (
-   <div>
-    <section className="bg-gradient-to-br from-yellow-800 to-yellow-400 h-screen py-[13%] px-56">
-     <h1 className="text-white font-bold text-4xl ">Lebih Cepat Lebih Baik</h1>
-     <p className="text-gray-400 mt-4">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-     </p>
-     <div className="flex mt-10 text-white gap-x-6">
-      <Link href="/">
-       <button className="rounded-full border px-3 py-2  hover:scale-110 duration-300 transition-all">
-        Hubungi Kami Sekarang
-       </button>
-      </Link>
-      <Link href="/">
-       <h1 className="hover:underline mt-2">Pelajari Selengkapnya</h1>
-      </Link>
+ return (
+  <div>
+   <section className="bg-gradient-to-br from-yellow-800 to-yellow-400 h-auto py-[13%] px-56">
+    <h1 className="text-white font-bold text-4xl ">Lebih Cepat Lebih Baik</h1>
+    <p className="text-gray-400 mt-4">
+     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+    <div className="flex mt-10 text-white gap-x-6">
+     <Link href="/">
+      <button className="rounded-full border px-3 py-2  hover:scale-110 duration-300 transition-all">
+       Hubungi Kami Sekarang
+      </button>
+     </Link>
+     <Link href="/">
+      <h1 className="hover:underline mt-2">Pelajari Selengkapnya</h1>
+     </Link>
+    </div>
+
+    {/* Keunggulan Section */}
+    <div className="text-center text-3xl mt-44 font-bold text-white">
+     <h1>Keunggulan Kami</h1>
+     <div className="flex flex-col md:flex-row justify-between mt-10">
+      <InstantProcess />
+      <Features />
      </div>
-    </section>
-    <section className="bg-gradient-to-br from-yellow-800 to-yellow-400 h-96">
-     <h1 className="text-center text-3xl py-10 font-bold text-white">
-      Keunggulan Kami
-     </h1>
-    </section>
-    <section className="h-screen">
-     <div className="text-center text-3xl py-10 font-bold text-white ">
-      <h1>Konsultasi Bisnis</h1>
-      <h1>Kebutuhan Anda</h1>
+     <div className="flex flex-col md:flex-row justify-between">
+      <MaxSpeed />
+      <CardList />
      </div>
-    </section>
-   </div>
-  );
+    </div>
+
+    {/* Keunggulan Section */}
+    <div className="text-center text-3xl mt-24 font-bold text-white">
+     <h1>Konsultasikan Bisnis Anda</h1>
+    </div>
+   </section>
+  </div>
+ );
 }
---
+++ components/CardList/MaxSpeed.tsx
@@ -0,0 +1,29 @@
+const MaxSpeed = () => {
+ return (
+  <div className="max-w-sm p-6 bg-transparent text-center">
+   <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="size-7 mx-auto text-bone">
+    <path
+     fillRule="evenodd"
+     d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z"
+     clipRule="evenodd"
+    />
+   </svg>
+   <div className="mt-2">
+    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-bone dark:text-bone">
+     Kecepatan yang Maksimal
+    </h5>
+    <p className="mb-3 font-normal text-sm text-bone dark:text-bone">
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mollitia animi
+     maiores ducimus facere consectetur cum. Facilis numquam amet quam doloribus
+     aspernatur, incidunt iste dolorem possimus unde porro totam consectetur?
+    </p>
+   </div>
+  </div>
+ );
+};
+
+export default MaxSpeed;
